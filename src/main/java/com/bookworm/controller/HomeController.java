f839fa2a400cbfb10d5f017bd994a12a4cff5fcc
ramendushandilya <ramendu.shandilya@gmail.com>
2018-01-22 18:00:46 +0530

    New functionalities addition

--- src/main/java/com/bookworm/controller/HomeController.java
+++ src/main/java/com/bookworm/controller/HomeController.java
@@ -236,6 +236,18 @@ public class HomeController {
     public String myProfile(Model model, Principal principal) {
         User user = userService.findByUsername(principal.getName());
         model.addAttribute("user", user);
+        model.addAttribute("userPaymentList", user.getUserPaymentList());
+        model.addAttribute("userShippingList", user.getUserShippingList());
+        model.addAttribute("orderList", user.getOrderList());
+
+        UserShipping userShipping = new UserShipping();
+        model.addAttribute("userShipping", userShipping);
+        model.addAttribute("listOfCreditCards", true);
+        model.addAttribute("listOfShippingAddresses", true);
+        List<String> stateList = USConstants.listOfUSStatesCode;
+        Collection.sort(stateList);
+        model.addAttribute("stateList", stateList);
+        model.addAttribute("classActiveEdit", true);
         return "myProfile";
     }
 
